7eeca8c17a304f256e4088bb553ebc4a784815c2
Rita-mary <amakomritamary322@gmail.com>
2026-08-19 14:21:17 +0100

    feat:Javascript Intro

---
+++ class_2/index.js
@@ -0,0 +1,18 @@
+let age = 17;
+
+let score = 40;
+
+let gender = "Girl"
+
+let name = " Rita-mary"
+console.log(score)
+console.log(name)
+console.log(age)
+
+let firstName = "Rita-mary"
+
+let middleName = "Ngozi"
+
+let lastName = "Amakom"
+
+console.log("My name is " + firstName + " " + middleName + " " + lastName + ". I am " + age + "years old. " + "I am a " + gender  + ".")
