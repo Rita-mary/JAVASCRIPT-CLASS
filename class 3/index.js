f18247dd7b6bb4e0921115f0a684d5673683f229
Rita-mary <amakomritamary322@gmail.com>
2026-08-21 12:37:59 +0100

    feat:calculations.

---
+++ class 3/index.js
@@ -0,0 +1,61 @@
+const gender = "Female"
+
+console.log(gender)
+
+let num1 =40
+let num2 =60
+
+let num3 = 3
+let num4 = 5
+
+let sum = num1 + num2
+let minus = num1 - num2
+let multiply = num1 * num2
+let divide = num1 / num2
+let power = num3 ** num4
+let mod = num4 % num3
+
+console.log(sum)
+console.log(minus)
+console.log(multiply)
+console.log(divide)
+console.log(power)
+console.log(mod)
+
+
+let sqr = Math.sqrt(9)
+let min = Math.min(34,45,123,123/5)
+let max = Math.max(34,45,123,123/5)
+
+console.log(sqr)
+console.log(max)
+console.log(Math.round(min))
+// rounding down
+console.log(Math.floor(min))
+// rounding up
+console.log(Math.ceil(29.3))
+// random numbers betweeon 1 and 100
+console.log(Math.round(Math.random(1,100)* 100))
+
+let email = "email@gmail.com"
+
+
+let age = 17;
+
+
+
+let firstName = "Rita-mary"
+
+let middleName = "Ngozi"
+
+let lastName = "Amakom"
+let fruit = "Mango"
+let color = "Red"
+let is_valid = true
+
+console.log("My name is " + firstName + " " + middleName + " " + lastName + ". I am " + age + "years old. " + "I am a " + gender  + " developer.")
+console.log(`My name is ${firstName} ${lastName}. I am a ${gender} developer`)
+
+console.log(`My best fruit is ${fruit} and my best colour is ${color}`)
+console.log(typeof(middleName))
+console.log(typeof is_valid )
