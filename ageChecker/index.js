fa862e0ec8d48b0885b404ed073a0a2c3fcb5e99
Rita-mary <amakomritamary322@gmail.com>
2026-08-27 18:00:53 +0100

    feat:intro to functions(addition)

--- ageChecker/index.js
+++ ageChecker/index.js
@@ -3,6 +3,7 @@
 // If the person is between 25 and 79,  He can rule
 // If the person is greater than 70, He is too old
 
+
 const ageEle = document.getElementById('ageInput');
 const ageButton = document.getElementById('ageBtn');
 
@@ -11,7 +12,7 @@ console.log(ageButton);
 
 ageButton.addEventListener('click', () => {
   inputVal = ageEle.value;
-  inputVal = Number(inputVal)
+  inputVal = Number(inputVal);
   if (inputVal < 25) {
     console.log("You're too young to rule");
   } else if (inputVal >= 25 && inputVal <= 70) {
@@ -22,3 +23,20 @@ ageButton.addEventListener('click', () => {
     console.log('Enter a valid age');
   }
 });
+
+// Assignment
+// learn about functions in js
+// do an add function
+
+let sum
+function add(x,y){
+  return x + y
+}
+
+sum =add(3,4)
+sum1 =add(35,49)
+sum2=add(63,34)
+
+console.log(sum, "Sum From the function defined")
+console.log(sum1, "Sum 1 From the function defined")
+console.log(sum2, "Sum 2 From the function defined")
